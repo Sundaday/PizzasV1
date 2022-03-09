be235d80949e86d8bf9787c685c9e740663e94f5
Sundaday <john.koch.pro@gmail.com>
2022-03-09 16:16:59 +0100

    add ingredients

---
+++ PizzasV1/Pizza.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzasV1
+{
+    internal class Pizza
+    {
+        #region Variable field
+        string name;
+        float price;
+        bool vegan;
+        #endregion
+
+        #region Constructor field
+        public Pizza() { }
+        public Pizza(string name, float price, bool vegan)
+        {
+            this.name = name;
+            this.price = price;
+            this.vegan = vegan;
+        }
+        #endregion
+
+        #region Function field
+        public void Afficher()
+        {
+            string badgeVege = vegan ? " (V)" : "";
+           
+            string nameDisplay = NameDisplay(name);
+
+            Console.WriteLine(nameDisplay + badgeVege + " - " + price + "€");
+        }
+        #endregion
+
+        private static string NameDisplay(string s)
+        {
+            string charMin = s.ToLower();
+            string charMaj = s.ToUpper();
+
+            string result = charMaj[0] + charMin[1..];
+            return result;
+        }
+    }
+}
--- PizzasV1/Program.cs
+++ PizzasV1/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace PizzasV1
-{
+{ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region UTF8
+            //Display "€" char
+            Console.OutputEncoding = Encoding.UTF8;
+            #endregion
+
+            var listePizzas = new List<Pizza>()
+            {
+                new Pizza("Royal", 11.3f, false),
+                new Pizza("Flamme", 13.5f, false),
+                new Pizza("siciLienne", 12.7f, false),
+                new Pizza("MontaGnarde", 14.3f, false),
+                new Pizza("KeBab", 11.5f, false),
+                new Pizza("AnchoiE", 10.5f, false),
+                new Pizza("FrOmage", 7.5f, true),
+                new Pizza("vegie", 12.5f, true)
+            };
+
+            foreach(var pizza in listePizzas)
+            {
+                pizza.Afficher();
+            }
+            
+
+            
+            
+
         }
     }
 }
